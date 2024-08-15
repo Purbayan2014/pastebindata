f19bbd9ea8e02d3187cd07c790d1d384fe6174fa
Purbayan <purbayan2014@gmail.com>
2024-08-16 05:26:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors
+
 const app = express();
 
+app.use(cors()); // Use cors middleware
 app.use(bodyParser.json());
 
 app.post('/log-credentials', (req, res) => {
@@ -18,4 +21,3 @@ app.post('/log-credentials', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
